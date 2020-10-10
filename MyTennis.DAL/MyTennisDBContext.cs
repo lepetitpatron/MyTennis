5ab359914891ffeafc09237998d0b458c7c2de3e
Hamza <menounyhamza@gmail.com>
2020-10-11 00:53:22 +0200

    Add EF Core

---
+++ MyTennis.DAL/MyTennisDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyTennis.DAL
+{
+    class MyTennisDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=.\SQLEXPRESS; initial catalog=MyTennis; integrated security=True;");
+        }
+    }
+}
