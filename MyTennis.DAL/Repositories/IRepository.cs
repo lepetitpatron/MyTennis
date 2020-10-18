b317f3f235920690a72e7f49b78e7b04d4dd596c
Hamza <menounyhamza@gmail.com>
2020-10-18 20:06:40 +0200

    Add Repositories

---
+++ MyTennis.DAL/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MyTennis.DAL.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class 
+    {
+        int Add(TEntity t);
+        TEntity FindById(int? id);
+        void Modify(TEntity t);
+        List<TEntity> GetAll();
+        void Remove(TEntity t);
+    }
+}
